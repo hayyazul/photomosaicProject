4929c6f8d1b60950c45921dd21905309bf7e23c7
Ayyazul Hassan <146155922+hayyazul@users.noreply.github.com>
2024-07-10 03:57:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from process_image_palette import PicturePalette
 
 from os.path import join
 
-INPUT_IMAGES = "C:/Users/Ayyaz/Pictures/iCloud Photos"
-TARGET_IMAGE = "C:/Users/Ayyaz/Pictures/632360_20220818163327_1.png"
+INPUT_IMAGES = "Your filepath to a file with photos. It may contain non-image elements as those are ignored by the program. Photos in subdirectories are not used."
+TARGET_IMAGE = "Filepath to an image."
 
 if __name__ == "__main__":
     photomosaic_maker = PhotomosaicPainter(join(INPUT_IMAGES), 1, 20000 * 10000)
